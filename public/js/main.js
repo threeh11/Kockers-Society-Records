ddc9e0afc8fda7ad009ebc9286ad5f3cead8a3fc
threeh <threeh1@vk.com>
2022-09-19 11:04:32 +0300

    fix change theme on site

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,69 @@
 let toggle = document.getElementById('toggle');
 let page = document.documentElement;
 
-if (localStorage.getItem('userTheme') == null) {
-    if (! toggle.classList.contains('dark')) {
-        switchMode();
-    } else {
-        // Определение темы браузера пользователя
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
-            page.classList.add('dark')
-            toggle.classList.add('dark')
-        } if (window.matchMedia('(prefers-color-scheme: dark)').matches == false) {
-            page.classList.remove('dark')
-            toggle.classList.remove('dark')
-        }
-    }
-} else {
-    page.classList.add(localStorage.getItem('userTheme'))
-    toggle.classList.add(localStorage.getItem('userTheme'))
+// Меняют общую тему сайта
+
+/**
+ * Устанавливает темную тему для сайта
+ *
+ * @returns {void}
+ */
+function setDark() {
+    localStorage.setItem('userTheme', 'dark');
+    page.classList.add('dark');
+    page.classList.remove('light');
+    toggle.classList.add('dark');
+    toggle.classList.remove('light');
 }
 
-console.log(localStorage.getItem('userTheme'));
+/**
+ * Устанавливает свтелую тему для сайта
+ *
+ * @returns {void}
+ */
+function setLight() {
+    localStorage.setItem('userTheme', 'light');
+    page.classList.remove('dark');
+    page.classList.add('light');
+    toggle.classList.remove('dark');
+    toggle.classList.add('light');
+}
 
+/**
+ * Меняет тему сайта
+ *
+ * @returns {void}
+ */
 function switchMode() {
+    localStorage.setItem('switch', 'off');
     if (! toggle.classList.contains('dark')) {
+        setDark();
+    } else {
+        setLight()
+    }
+}
+
+if (localStorage.getItem('switch') == 'on') {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
         localStorage.setItem('userTheme', 'dark');
-        page.classList.add(localStorage.getItem('userTheme'))
-        toggle.classList.add(localStorage.getItem('userTheme'))
+    }
+}
+
+if (localStorage.getItem('userTheme') == null) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
+        localStorage.setItem('userTheme', 'dark');
     } else {
         localStorage.setItem('userTheme', 'light');
-        page.classList.add(localStorage.getItem('userTheme'))
-        toggle.classList.add(localStorage.getItem('userTheme'))
     }
-};
+}
+
+if (localStorage.getItem('userTheme') == 'dark') {
+    setDark();
+} else {
+    setLight();
+}
+
+
+
+//
+
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -33,7 +33,7 @@
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 stroke-bg dark:stroke-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-        </svg>          
+        </svg>
     </div>
 </header>
 
@@ -59,6 +59,6 @@
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 stroke-bg dark:stroke-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-        </svg>          
+        </svg>
     </div>
 </header>
